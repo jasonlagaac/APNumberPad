557e89f53def6e00e980bdbbb81eff20c3ab1f1b
Basil Shkara <basil@neat.io>
2017-01-04 18:28:09 +0100

    Added framework target for use with Carthage.

---
+++ APNumberPadiOS/APNumberPadiOS.h
@@ -0,0 +1,19 @@
+//
+//  APNumberPadiOS.h
+//  APNumberPadiOS
+//
+//  Created by Basil Shkara on 4/1/17.
+//  Copyright © 2017 Andrew Podkovyrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APNumberPadiOS.
+FOUNDATION_EXPORT double APNumberPadiOSVersionNumber;
+
+//! Project version string for APNumberPadiOS.
+FOUNDATION_EXPORT const unsigned char APNumberPadiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APNumberPadiOS/PublicHeader.h>
+
+
